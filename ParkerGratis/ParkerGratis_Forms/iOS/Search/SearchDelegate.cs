fa465ae3b3d59a64f7c3047d9ae60f402cbe4ccb
Leif Larsen <me@leiflarsen.org>
2015-05-04 18:46:38 +0200

    Forgot this file.

---
+++ ParkerGratis/ParkerGratis_Forms/iOS/Search/SearchDelegate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UIKit;
+using MapKit;
+using Foundation;
+using System.Linq;
+
+namespace ParkerGratis_Forms_iOS.iOS
+{
+	public class SearchDelegate : UISearchDisplayDelegate
+	{
+		public SearchDelegate ()
+		{
+		}
+
+		public override bool ShouldReloadForSearchString(UISearchDisplayController controller, string forSearchString)
+		{
+			var searchRequest = new MKLocalSearchRequest ();
+			searchRequest.NaturalLanguageQuery = forSearchString;
+
+			var localSearch = new MKLocalSearch (searchRequest);
+			localSearch.Start (delegate (MKLocalSearchResponse response, NSError error) {
+				if(response != null && error == null) {
+					//Console.WriteLine (response);
+					((SearchSource)controller.SearchResultsSource).mapItems = response.MapItems.ToList();
+					controller.SearchResultsTableView.ReloadData();
+				} else {
+					//Console.WriteLine (error);
+				}
+			});
+
+			return false;
+		}
+	}
+}
+
